7266313b2859a8a48871b321c00f799b06ad4285
rpadebet <rohit.pittu@gmail.com>
2017-04-16 10:06:35 -0400

    Dates cleaned up

--- Test scripts.R
+++ Test scripts.R
@@ -5,4 +5,29 @@ loan_lc_dt$earliest_cr_line<-gsub("^","01-",x = loan_lc_dt$earliest_cr_line)
 
 head(loan_lc$int_rate)
 
+library(stringi)
+dates <- loan_lc_sub$earliest_cr_line
+
+dates_len<-sapply(dates,str_length)
+dates_df<-as.data.frame(cbind(dates,dates_len))
+
+
+clean_dates<-function(d){
+    
+    if(str_length(d)==6){
+        d1<-as.Date(gsub("^","01-",x=d),format="%d-%b-%y")
+    }
+    else {
+        d1<-as.Date(gsub("^","01-",x=d),format="%d-%b-%Y")
+    }
+    return(d1)
+}
+
+clean_history<-as_date(ifelse(stri_length(loan_lc_sub$earliest_cr_line)==6,
+                                             as.Date(gsub("^","01-",x=loan_lc_sub$earliest_cr_line),format="%d-%b-%y"),
+                                             as.Date(gsub("^","01-",x=loan_lc_sub$earliest_cr_line),format="%d-%b-%Y") ))
+
+
+head(dates_df[dates_len==6,],100)
+
 
