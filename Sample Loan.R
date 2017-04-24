4eee361fcd11d4add2294d7f38ea4ec21633bd71
rpadebet <rohit.pittu@gmail.com>
2017-04-24 19:03:00 -0400

    All states sample loans

--- Sample Loan.R
+++ Sample Loan.R
@@ -25,5 +25,28 @@ N<-length(loan_ids)
 write.csv(loan_dt,"Sample Loans CA 36 2012_2015.csv")
 
 
+#####################################################
+# state <- "CA"
+vintage <-c(2011:2015)
+TERM <-"36 months"
+sample_size<-100000
 
+loan_mod_s<-loan_mod[(VINTAGE %in% vintage)&(term == TERM),]
+
+## Sampling Loans
+loan_ids<-if(length(loan_mod_s$id)>sample_size){
+    sample(loan_mod_s$id,sample_size)
+} else{
+    sample(loan_mod_s$id,length(loan_mod_s$id))
+}
+
+
+
+## Select data by vintage, term and state
+loan_dt<-loan_mdl[(id %in% loan_ids),]
+
+N<-length(loan_ids)
+
+
+write.csv(loan_dt,"Sample Loans 36 2011_2015.csv")
 
