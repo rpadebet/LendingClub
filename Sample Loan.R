1025dd463af2c42a29f96801b5e7648428118a71
rpadebet <rohit.pittu@gmail.com>
2017-04-24 22:47:13 -0400

    Sample 2012-2013Q3

--- Sample Loan.R
+++ Sample Loan.R
@@ -46,12 +46,12 @@ loan_dt<-loan_mdl[(id %in% loan_ids),]
 write.csv(loan_dt,"Sample Loans 36 2011_2015.csv")
 
 #####################################################
-# Loan from all States 2012H1 - 36 months
+# Loan from all States Jan-2012 to Sep-2013 - 36 months
 #####################################################
 # state <- "CA"
 # vintage <-c(2011:2015)
 issue<-seq(as.Date("2012-01-01",origin=Sys.Date()), 
-           as.Date("2012-06-01",origin=Sys.Date()),
+           as.Date("2013-09-01",origin=Sys.Date()),
            by= "month")
 TERM <-"36 months"
 sample_size<-10000
@@ -68,5 +68,6 @@ loan_ids<-if(length(loan_mod_s$id)>sample_size){
 ## Select data by vintage, term and state
 loan_dt<-loan_mdl[(id %in% loan_ids),]
 
-write.csv(loan_dt,"Sample Loans 36 2012H1.csv")
+write.csv(loan_dt,"Sample Loans 36 2012_2013.csv")
+
 
